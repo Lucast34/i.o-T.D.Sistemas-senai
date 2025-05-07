b26e652b57a3e9a720e0edef648fc50d84a98648
L <lucast34t@gmail.com>
2025-05-07 11:57:52 -0300

    backup

---
+++ Lucas2/src/main/java/com/mycompany/lucas/Lucas.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lucas;
+
+import java.util.Scanner;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.io.FileWriter;
+import java.io.FileReader;
+
+
+/**
+ *
+ * @author ead
+ */
+public class Lucas {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Quantas pessoas cadastrar? ");
+        
+        int qtd = input.nextInt();
+        
+        input.nextLine();
+        
+        Pessoa[] pessoas = new Pessoa[qtd];
+        
+        for(int i=0;i<qtd;i++){
+            System.out.println("\n Cadastro de Pessoa"+(i+1)+"---");
+            
+            System.out.println("Nome: ");
+            String nome = input.nextLine();
+            
+            System.out.println("CPF: ");
+            String cpf = input.nextLine();
+            
+            System.out.println("Data Nascimento (AAAA-MM-DD): ");
+            String dataStr = input.nextLine();
+            
+            System.out.println("Altura (em metros): ");
+            double altura = input.nextDouble();
+        
+            System.out.println("Altura (em KG): ");
+            double peso = input.nextDouble();
+            
+            LocalDate dataLocal = LocalDate.parse(dataStr);
+            
+            pessoas[i] = new Pessoa(nome,cpf,dataLocal,altura,peso);
+        }
+        
+        try (FileWriter write = new FileWriter("pessoa.txt")){
+            write.write("Nome;Cpf:dataNascimento;Altura.;Peso;Imc;MaiorIdade\n");
+            for(Pessoa p: pessoas){
+                write.write(p.toCSV()+"\n");
+            }
+            System.out.println("\nArquivo 'pessoas.txt salvo com sucesso'");
+        }catch(IOException e){
+            System.out.println("Error:"+e.getMessage());
+        }
+        input.close();
+    }  
+}
