0f25e5212e86d5996f600573f02089cc15a62056
Lucast34 <lucast34t@gmail.com>
2025-05-20 15:41:03 -0300

    cinema

--- java/jdk-24/Cinema/src/cinema/ConnectionFactory.java
+++ java/jdk-24/Cinema/src/cinema/ConnectionFactory.java
@@ -4,10 +4,26 @@
  */
 package cinema;
 
-/**
- *
- * @author luy
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionFactory {
-    
+    public Connection connectaBD(){
+        Connection conn = null;
+        
+        try {
+            
+            String url = "jdbc:mysql://localhost:3306/db_cinema?useSS1=false";
+            String user = "root";
+            String password = "";
+            
+            conn = DriverManager.getConnection(url, user, password);
+            
+        } catch (SQLException e) {
+            System.out.println("Error: "+e.getMessage());
+            
+        } 
+        return conn;
+    }
 }
