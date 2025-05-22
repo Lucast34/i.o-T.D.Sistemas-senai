dbf1e9d12cb2470751b01eba982843bae0bdf9fe
Lucast34 <lucast34t@gmail.com>
2025-05-22 09:40:23 -0300

    cadastro

---
+++ java/jdk-24/Cadastro/src/cadastro/Pessoa.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cadastro;
+
+/**
+ *
+ * @author ead
+ */
+public abstract class Pessoa {
+    private String nome;
+    private String endereco;
+    private String sexo;
+    private String cpf;
+
+    public Pessoa(String nome, String endereco, String sexo, String cpf) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.sexo = sexo;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    
+    
+    
+}
