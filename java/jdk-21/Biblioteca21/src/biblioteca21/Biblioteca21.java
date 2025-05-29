3cd8e5f40bb406e6005a1e6625fe5856b8d0bf64
Lucast34 <lucast34t@gmail.com>
2025-05-28 21:00:56 -0300

    test crud

--- java/jdk-21/Biblioteca21/src/biblioteca21/Biblioteca21.java
+++ java/jdk-21/Biblioteca21/src/biblioteca21/Biblioteca21.java
@@ -1,24 +1,56 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * Click nbfs:nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs:nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package biblioteca21;
 
 import java.sql.SQLException;
+import java.util.List;
 
 
-private final class userDAO
+
 
 public class Biblioteca21 {
-
-    /**
-     * @param args the command line arguments
-     */
+    
+    
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+         UsuarioDAO userDAO = new UsuarioDAO();
         
         ConnectionFactory connF = new ConnectionFactory();
         connF.connectaDB();
+        
+        Usuario usr = new Usuario(0, "TestName", "Test@gmail.com", "61 95555555"
+               ,"Desativado");
+        
+        try {
+            userDAO.criarUsuario(usr);
+            System.out.println("Adicionado");
+            
+        } catch (SQLException e) {
+            System.out.println("Erro: "+ e.getMessage());
+        }   
+        
+        try {
+            List<Usuario> usuarios = userDAO.listarUsuarios();  
+            if(usuarios.isEmpty()){
+                System.out.println("A lista esta Vazia");
+            }else{
+                for(Usuario usuario : usuarios){
+                    System.out.println("ID: "+usuario.getId());
+                    System.out.println("Nome: "+usuario.getNome());
+                    System.out.println("Email: "+usuario.getEmail());
+                    System.out.println("Telefone: "+usuario.getTelefone());
+                    System.out.println("Tipo: "+usuario.getTipo_usuario());
+                    System.out.println("________________X_______________");
+                }
+            }
+            
+        } catch (SQLException e) {
+            System.out.println("Erro: "+e.getMessage());
+            
+        }
         
     }
     
