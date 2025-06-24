2a4150880fbcb226b5b05dcc348fe6f6294da29e
Lucast34 <lucast34t@gmail.com>
2025-06-24 11:30:15 -0300

    java swing

--- java/jdk-24/BibliotecaSwing/src/bibliotecaswing/UsuarioDAO.java
+++ java/jdk-24/BibliotecaSwing/src/bibliotecaswing/UsuarioDAO.java
@@ -5,10 +5,23 @@
 package bibliotecaswing;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 public class UsuarioDAO {
     private Connection connection;
     
-    public UsuarioDAO
+    public UsuarioDAO(){
+        this.connection = new ConnectionFactory().connectaBD();
+    }
+    
+    public void criarUsuario(Usuario u) throws SQLException{
+        
+        String sql = "INSERT INTO tb_usuario(nome,email,telefone,tipo_usuario) "
+                + "VALUES(?,?,?,?)";
+        
+        try{}catch{}finally{}
+    
+    
+    }
     
 }
